0b6948c680fcb9b17ba9b3c34e2cb331772e65b9
WesleyLFernandes <61124547+WesleyLFernandes@users.noreply.github.com>
2023-05-30 05:59:24 -0300

    atualização

--- Sprint8/tarefa1/projetowesley.py
+++ Sprint8/tarefa1/projetowesley.py
@@ -5,39 +5,35 @@ import json
 
 api_key = "838b6bf7ad091a6b1ea4100e79e1377a"
 
-#url = f"https://api.themoviedb.org/3/movie/top_rated?api_key={'838b6bf7ad091a6b1ea4100e79e1377a'}&language=pt-BR"
+url = f"https://api.themoviedb.org/3/discover/movie?api_key={'838b6bf7ad091a6b1ea4100e79e1377a'}&include_adult=false&include_video=false&with_genres=16,35&language=en-US&release_date.gte=2013&release_date.lte=2023"
 
-url = f"https://api.themoviedb.org/3/discover/movie?api_key={'838b6bf7ad091a6b1ea4100e79e1377a'}&include_adult=false&include_video=false&with_genres=16"
-
-response = requests.get(url)
+response = requests.get(url) 
 data = response.json()
-filmes = []
+directors = []
+numero_paginas = data['total_pages']
 
-for movie in data['results']:
-    df = {'Id':movie['id'],
-    'Titulo': movie['title'],
-    'Média de votos:': movie['vote_average']
-    }
-    filmes.append(df)
-df = pd.DataFrame(filmes)
-#display(df)
 
-qtd = len(filmes)
+for pagina in range(1, numero_paginas+1):
+    url = f"https://api.themoviedb.org/3/discover/movie?api_key={'838b6bf7ad091a6b1ea4100e79e1377a'}&include_adult=false&include_video=false&with_genres=16,35&language=en-US&release_date.gte=2013&release_date.lte=2023&page={pagina}"
+    response = requests.get(url) 
+    data = response.json()    
 
-for z in range(qtd):
-    identifica = filmes[z]["Id"]  # valor id de cada filme na pagina
-    url = f"https://api.themoviedb.org/3/movie/{identifica}/credits?api_key={'838b6bf7ad091a6b1ea4100e79e1377a'}"
-   
-    response = requests.get(url)
-    dados2 = response.json()    
-    directors = []  
-    
-    for credit in dados2['crew']:        
-        if credit['job'] == 'Director':            
-            for job, value_of_dict in credit.items():
-                if job == 'name':
-                    df = {'Diretor': value_of_dict,
-                    'ID filme': filmes[z]["Id"]
-                    }                                   
-                    with open("df.json", "a+") as arquivo:     
-                        json.dump(df, arquivo, indent=4)
+    for movie in data['results']:
+                        
+        url = f"https://api.themoviedb.org/3/movie/{movie['id']}/credits?api_key={'838b6bf7ad091a6b1ea4100e79e1377a'}"
+                
+        response2 = requests.get(url)
+        data2 = response2.json()                
+                    
+        for credit in data2['crew']:        
+            if credit['job'] == 'Director':            
+                for job, value_of_dict in credit.items():
+                    if job == 'name':
+                        df = {'Diretor': value_of_dict,
+                        'ID filme': movie['id']
+                        }
+                        directors.append(df)
+    print(f"Pagina: {pagina}")                     
+print("Acabei!")
+with open("Diretores.json", "a+") as arquivo:     
+    json.dump(directors, arquivo, indent=4)                                                
